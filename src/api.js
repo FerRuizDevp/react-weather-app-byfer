1e6b1f6b6a7908f645def816c08c02debcef5ff4
Fernanda Ruiz <117100019+FerRuizDevp@users.noreply.github.com>
2025-03-13 00:03:11 -0400

    Editing variable names

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 export const geoApiOptions = {
   method: "GET",
   headers: {
-    "x-rapidapi-key": process.env.REACT_WEATHER_APP_GEODB - CITIES_API_KEY,
+    "x-rapidapi-key": process.env.REACT_WEATHER_APP_GEODBCITIES_API_KEY,
     "x-rapidapi-host": "wft-geo-db.p.rapidapi.com",
   },
 };
@@ -11,4 +11,4 @@ export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities";
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5/";
 
 export const WEATHER_API_KEY =
-  process.env.REACT_WEATHER_APP_OPEN - WEATHER_API_KEY;
+  process.env.REACT_WEATHER_APP_OPENWEATHER_API_KEY;
